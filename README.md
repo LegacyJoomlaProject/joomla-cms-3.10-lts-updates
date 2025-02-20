3372e92920a21e2286fd7b1235d6a22ffc394f03
LegacyJoomlaProject <itregister@legacyjoomla.com>
2025-02-19 17:53:44 -0800

    Apply code changes from Joomla_3.10.20-ljp-Stable-Update_Package to source tracking directory.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Public release of Legacy Joomla Project 3rd party mantenance/security updates for the Joomla! V3.10 CMS.
 
 ## Current update package:
-Version 3.10.19-ljp-pkg : Derived from the Joomla! eLTS release 3.10.19-elts.
+Version 3.10.20-ljp-pkg : Derived from the Joomla! eLTS release 3.10.20-elts.
 
 ## Installing the updates:
 Updates are provided as a public service without warranty. <br>
--- src/libraries/src/Version.php
+++ src/libraries/src/Version.php
@@ -49,7 +49,7 @@ final class Version
 	 * @var    integer
 	 * @since  3.8.0
 	 */
-	const PATCH_VERSION = 19;
+	const PATCH_VERSION = 20;
 
 	/**
 	 * Extra release version info.
@@ -78,7 +78,7 @@ final class Version
 	 * @since  3.5
 	 * @deprecated  4.0  Use separated version constants instead
 	 */
-	const DEV_LEVEL = '19-ljp';
+	const DEV_LEVEL = '20.1-ljp';
 
 	/**
 	 * Development status.
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '15-December-2024';
+	const RELDATE = '20-January-2024';
 
 	/**
 	 * Release time.
